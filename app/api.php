aba328cb31a8dc3024a9f8ceed6ef4d7b7400cfd
BuqiLiao <liaobuqi2022@163.com>
2023-05-07 23:57:45 -0700

    Latest Update for comments 	modified:   app/api.php 	modified:   app/index.html 	modified:   app/index.js

--- app/api.php
+++ app/api.php
@@ -1,41 +1,53 @@
 <?php
+// Set content type and headers to allow cross-origin requests
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
 
+// Create a new PDO connection
 $pdo = new PDO('sqlite:comments.db');
-$pdo->exec("CREATE TABLE IF NOT EXISTS forms (id INTEGER PRIMARY KEY, username TEXT, favorite_subject TEXT, favorite_book_title TEXT)");
 
+// Get request method and path
 $method = $_SERVER['REQUEST_METHOD'];
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $path = explode('/', $path);
 
-switch ($method) {
+switch($method){
+  // Handle GET requests
   case 'GET':
-    if (isset($path[3]) && isset($path[4])) {
+    // Check if the book_id is set in the path
+    if(isset($path[3]) && isset($path[4])){
+        // Create the book_id variable
         $book_id = "/".$path[3]."/".$path[4];
+        // Select comments by the book_id
         $stmt = $pdo->prepare('SELECT * FROM comments WHERE book_id = ?');
         $stmt->execute([$book_id]);
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // Convert results to json format
         echo json_encode($rows);
         break;
-    } else {
+    }else{
+        // If book_id is not set, select all comments
         $stmt = $pdo->prepare('SELECT * FROM comments');
         $stmt->execute();
         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
         echo json_encode($rows);
         break;
     }
+  // Handle POST requests    
   case 'POST':
+    // Decode data from the request body and store it
     $data = json_decode(file_get_contents('php://input'), true);
-    // file_put_contents('/var/www/myapp/log.txt', print_r($data, true));
+    // Insert record into the database
     $stmt = $pdo->prepare('INSERT INTO comments (book_id, username, comment) VALUES (?, ?, ?)');
     $stmt->execute([$data['book_id'], $data['username'], $data['comment']]);
 
+    // Return the lastInsertId in json format
     echo json_encode(['id' => $pdo->lastInsertId()]);
     break;
 
   default:
+    // If a different method is used, return an error
     http_response_code(405);
     echo json_encode(['error' => 'Method Not Allowed']);
     break;
--- app/index.js
+++ app/index.js
@@ -1,46 +1,64 @@
 $(document).ready(function () {
+    //Get the search form and add a submit event listener
     $("#search-form").on("submit", function (e) {
-        e.preventDefault();
-        const query = $("#search").val();
-        searchBooks(query);
-    });
-    $("#results").on("click", ".add-comment-btn", function () {
-        console.log("clicked");
-        const bookId = $(this).data("book-id");
+        e.preventDefault(); //prevent the default action
 
+        const query = $("#search").val();   //get the value of the search field
+        searchBooks(query); //call the searchBooks() method passing in the search query
+    });
+
+    // Add click event listener on add comment button
+    $("#results").on("click", ".add-comment-btn", function () {
+        // get the data-book-id attribute from the clicked element and set it to bookId
+        const bookId = $(this).data("book-id");
+        //set the value of the book-id input field to the bookId
         $("#book-id").val(bookId);
+        //show the comment modal
         $("#comment-modal").modal("show");
     });
 
+    //Add submit event listener on the comment form  
     $("#comment-form").on("submit", function (e) {
         e.preventDefault();
-        submitComment();
+
+        submitComment();    //call the submitComment() method 
     });
 
+    //Add click event listener on the view comments button
     $("#results").on("click", ".view-comments", function () {
-        const bookId = $(this).data("id");
-        const commentsContainer = $(this).siblings(".comments-container");
+        //get the data-id attribute(book-id) from the clicked element
+        let bookId = $(this).data("id");
+        //get the list group which holds comments for current book
+        let commentsContainer = $(this).siblings(".comments-container");
+        
+        //check if the list group is visible
         if (commentsContainer.is(":visible")) {
+            //if visible, hide it
             commentsContainer.hide();
         } else {
+            //make an ajax call the api using /GET/{id}
             $.ajax({
                 url: "api.php/"+bookId,
                 type: "GET",
                 success: function (data) {
                     console.log(data);
                     let html = "";
+                    //check if no comments yet
                     if(data.length === 0) {
                         html += `<div class="comment">
                                     <p>No comments yet.</p>
                                 </div>`;
                     }else{
+                        //iterate through each comment and generate the HTML
                         data.forEach((comment) => {
                         html += `<div class="comment">
                                     <p><strong>${comment.username}</strong> - ${comment.comment}</p>
                                 </div>`;
                     });
                     }
+                    //update comments container with generated html
                     commentsContainer.html(html);
+                    //show the comments container
                     commentsContainer.show();
                 },
                 error: function () {
@@ -51,29 +69,42 @@ $(document).ready(function () {
     });
 
 });
+
+//Add click event listener on the view all comments button
 $("#view-all-comments").on("click", function () {
+    //check if the comments-container is visible
     if ($(".comments-container").is(":visible")) {
         $(".comments-container").hide();
     } else {
+        //make an ajax call to the api using /GET
         $.ajax({
         url: "api.php",
         type: "GET",
         success: function (data) {
+            //Object to hold all comments grouped by their respective book_id
             const commentsByBookId = {};
 
+            // iterate through each comment and update the commentsByBookId object
             data.forEach((comment) => {
                 if (!commentsByBookId[comment.book_id]) {
                 commentsByBookId[comment.book_id] = [];
                 }
                 commentsByBookId[comment.book_id].push(comment);
             });
-            console.log(commentsByBookId);
+            // console.log(commentsByBookId);
+
+            // iterate through each book card in results and update its comments html
             $(".book-card").each(function () {
+                //get the book id from the add comment button
                 let bookId = $(this).find(".add-comment-btn").data("book-id");
+                //get the comments container of the current book card
                 const commentsContainer = $(this).find(".comments-container");
-                console.log(bookId);
+                // console.log(bookId);
+                
+                // check if comments exist for the current book card
                 if (commentsByBookId[bookId]) {
                     let html = "";
+                    //generate the html for each comment
                     commentsByBookId[bookId].forEach((comment) => {
                         html += `<div class="comment">
                                     <p><strong>${comment.username}</strong> - ${comment.comment}</p>
@@ -81,8 +112,10 @@ $("#view-all-comments").on("click", function () {
                     });
                     commentsContainer.html(html);
                 } else {
+                    //update the comments container with a message indicating no comments yet
                     commentsContainer.html("<p>No comments yet.</p>");
                 }
+                //show the comments container
                 commentsContainer.show();
             });
         },
@@ -92,12 +125,17 @@ $("#view-all-comments").on("click", function () {
         });
     }
 });
+
+//Fetch books and display them
 function searchBooks(query) {
+    //get the api url and pass the query param
     const apiUrl = "https://openlibrary.org/search.json?q=" + encodeURIComponent(query);
+    // make an ajax call to the api
     $.ajax({
         url: apiUrl,
         type: "GET",
         success: function (data) {
+            // call the displayResults() method and pass the requested book results
             displayResults(data.docs);
         },
         error: function (error) {
@@ -106,12 +144,15 @@ function searchBooks(query) {
     });
 }
 
+//Display the results in a card layout
 function displayResults(books) {
-    if (books.length === 0) {
+    if (books.length === 0 || books === undefined || books === null) {
+        //if no books found, display an appropriate message
         $("#results").append("<p>No results found</p>");
         return;
     } else {
         let html = "";
+        //iterate through each book result and generate the html card
         books.forEach((book) => {
             html += `
             <div class="book-card card">
@@ -127,17 +168,21 @@ function displayResults(books) {
                 </div>
             </div>`;
         });
+        //update the results div with the generated html cards
         $("#results").html(html);
     }
 }
 
+// Submit comment for a book
 function submitComment() {
+    // Get book id, username and comment from the form
     const bookId = $("#book-id").val();
     const username = $("#username").val();
     const comment = $("#comment").val();
 
+    // Make a POST request to the API endpoint
     $.ajax({
-        url: "api.php", // Replace with your API endpoint
+        url: "api.php",
         type: "POST",
         data: JSON.stringify({
             book_id: bookId,
@@ -146,7 +191,12 @@ function submitComment() {
         }),
         contentType: "application/json",
         success: function (data) {
-            alert("Comment submitted successfully!");
+            if(data.length === 0) {
+                alert("An error occurred while submitting the comment.");
+            }else{
+                alert("Comment submitted successfully!");
+            }
+            // Hide the modal
             $("#comment-modal").modal("hide");
         },
         error: function () {
