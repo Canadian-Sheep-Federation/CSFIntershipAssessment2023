62e89fe785bb05eeb10d8aed4bb7f62ff52f68b3
Nithushan Selvanesan <nithushan.selvanesan@mail.utoronto.ca>
2023-05-08 01:55:07 -0400

    Added QA Assessment and API endpoints for Software Developer Assessment

---
+++ SoftwareDeveloperAssessment/API/database.js
@@ -0,0 +1,18 @@
+var sql3 = require('sqlite3').verbose()
+
+const dbsource = "db.sqlite"
+
+let db = new sql3.Database(dbsource, (err) => {
+    if (err) {
+        console.error(err.message)
+        throw err
+    } else {
+        console.log("Connected to the database!")
+        // Store the field information in this database
+        db.run(`CREATE TABLE Users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        author TEXT,
+        title TEXT,
+        line TEXT)`)
+    }
+})
---
+++ SoftwareDeveloperAssessment/API/server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import db from "./database.js";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+// This api is running on port 3000
+app.listen(3000, () => {
+    console.log("The server is noww running on port 3000")
+});
+
+// Get all the user form information
+app.get('/', (req, res) => {
+    db.all("SELECT * FROM Users", [], (err, rows) => {
+        if (err) {
+            res.status(400)
+        } else {
+            res.status(200)
+            res.json({
+                "data": rows
+            })
+        }
+    })
+});
+
+// Get a specific user's info
+app.get('/:id', (req, res) => {
+    db.get("SELECT * FROM Users WHERE id = ?", [req.params.id], (err, row) => {
+        if (err) {
+            res.status(400)
+        } else {
+            res.status(200)
+            res.json({
+                "data": row
+            })
+        }
+    })
+});
+
+// Receive a specific user's info
+app.post('/', (req, res) => {
+    if (!req.body.author || !req.body.title || !req.body.line) {
+        res.status(400)
+    } else {
+        db.run("INSERT INTO Users (author, title, line) VALUES (?, ?, ?)", [req.body.author, req.body.title, req.body.line], function (err, result) {
+            if (err) {
+                res.status(400)
+            } else {
+                res.status(200)
+                res.json({ "id": this.lastID })
+            }
+        })
+    }
+});
