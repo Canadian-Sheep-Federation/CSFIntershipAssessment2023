c37a18f6022a007b89a0445da04a9dde88e9f149
1moonala <junseomoon9@gmail.com>
2023-05-09 11:19:55 -0400

    Implemented express backend server and integrated sqlite3 database

--- app/backend/index.js
+++ app/backend/index.js
@@ -1,8 +1,61 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./database");
+
 const app = express();
 const port = 8888;
-const axios = require("axios");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(cors())
+// Returns all existing surveys
+app.get("/", (req, res) => {
+  db.all("SELECT * FROM survey", (err, rows) => {
+    if (err) {
+      res.status(400);
+    } else {
+      res.send({ data: { surveys: rows } });
+      res.status(200);
+    }
+  });
 
-app.get
+  return;
+});
+
+// Returns survey data for corresponding id param
+app.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.get("SELECT * FROM survey WHERE id = ?", [id], (err, row) => {
+    if (err) {
+      res.status(400);
+    } else {
+      res.send({ data: { survey: row } });
+      res.status(200);
+    }
+  });
+
+  return;
+});
+
+// Inserts new survey entry into database
+app.post("/", (req, res) => {
+  const data = req.body;
+
+  db.run(
+    "INSERT INTO survey (email, showName, rating) values(?, ?, ?)",
+    [data.email, data.showName, data.rating],
+    (err) => {
+      if (err) {
+        res.status(400);
+      } else {
+        res.status(201);
+      }
+    }
+  );
+
+  return;
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}!`);
+});
